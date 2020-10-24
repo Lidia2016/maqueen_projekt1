92a2d4f04e0b4d5cb54d77a12993ccf03ee41e00
Lidia2016 <L.Aparta@gmail.com>
2020-10-24 13:27:26 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
+    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
+    basic.pause(1000)
+    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+    basic.pause(1000)
+    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 150)
+    maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 150)
+    basic.pause(1000)
+})
